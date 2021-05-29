765bc1a6fdb1e5017ba6ee50221b131265346f66
boeing7478F <85016918+boeing7478F@users.noreply.github.com>
2021-05-29 17:17:59 +0530

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,9 @@
+input.onButtonPressed(Button.A, function () {
+    music.playMelody("C5 B A G F E D C ", 455)
+})
+input.onButtonPressed(Button.B, function () {
+    music.stopAllSounds()
+})
+basic.forever(function () {
+	
+})
